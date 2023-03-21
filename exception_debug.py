7282485578db93f972873f9b1daa78015a2c9e5d
Vamshi-Munukuntla <vamshi.kumar59@gmail.com>
2023-03-21 23:58:02 +0530

    Project Template is ready

--- exception_debug.py
+++ exception_debug.py
@@ -8,18 +8,18 @@ try:
     x2 = (-b - math.sqrt(b * b - 4 * a * c)) / (2 * a)
     print(x1, x2)
 except:
-    e, p, t = sys.exc_info()
-    print('E:', e)
-    print('P:', p)
-    print('T', t)
-    print('ERROR FILE PATH:', t.tb_frame)
-    print('ERROR LINE NUMBER:', t.tb_frame.f_lineno)
-    print('TRY BLOCK STARTING LINE NUMBER:', t.tb_lineno)
-    print('FILE_NAME:', t.tb_frame.f_code.co_filename)
+    e, p, exec_tb = sys.exc_info()
+    print('TYPE OF EXCEPTION:', e)
+    print('EXCEPTION ITSELF:', p)
+    print('TRACEBACK OBJECT', exec_tb)
+    print('ERROR FILE PATH:', exec_tb.tb_frame)
+    print('ERROR LINE NUMBER:', exec_tb.tb_frame.f_lineno)
+    print('TRY BLOCK STARTING LINE NUMBER:', exec_tb.tb_lineno)
+    print('FILE_NAME:', exec_tb.tb_frame.f_code.co_filename)
 
-    exception_block_line_number = t.tb_frame.f_lineno
-    try_block_line_number = t.tb_lineno
-    file_name = t.tb_frame.f_code.co_filename
+    exception_block_line_number = exec_tb.tb_frame.f_lineno
+    try_block_line_number = exec_tb.tb_lineno
+    file_name = exec_tb.tb_frame.f_code.co_filename
 
     error_message = f"""
     Error occurred in script: 
